fa2f87d9b8ff46aaf45fb7b08678a1dbd6c8d44e
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2016-02-26 16:20:00 +0300

    [ADD] note_issue_reason.py

---
+++ addons/l10n_in_gst/models/note_issue_reason.py
@@ -0,0 +1,23 @@
+# Part of actpy See LICENSE file for full copyright and licensing details.
+
+""" Introduce following new models for GST related customization:
+        * Note Issue Reasons
+"""
+
+from actpy import api, fields, models
+
+
+class NoteIssueReason(models.Model):
+    _name = 'note.issue.reason'
+
+    name = fields.Char(string='Name', required=True)
+    code = fields.Char(string='Code', required=True)
+
+    @api.multi
+    def name_get(self):
+        """ It will display field data in proper format as specified in this
+        method """
+        result = []
+        for record in self:
+            result.append((record.id, '%s-%s' % (record.code, record.name)))
+        return result
