681fa47c792e55a3d90198d38f05570c9a7bdffc
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2016-11-13 21:40:00 +0300

    [ADD] sales_team.py

---
+++ addons/rma/models/sales_team.py
@@ -0,0 +1,19 @@
+# Part of actpy See LICENSE file for full copyright and licensing details.
+
+from actpy import fields, models
+
+
+class CrmTeam(models.Model):
+    _inherit = 'crm.team'
+
+    use_return = fields.Boolean(string='Return')
+    returns_count = fields.Integer(
+        compute='_compute_returns',
+        string='Number of returns')
+
+    def _compute_returns(self):
+        for return_req in self:
+            if return_req.use_return:
+                rma_ids = self.env['rma.request'].search([
+                    ('team_id.id', '=', return_req.id)])
+                return_req.returns_count = len(rma_ids)
