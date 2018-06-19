9394bae6473a3a320ddd603543b6f9fea61f8150
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:45:48 +0200

    Create new addons file : addons/sms/__manifest__.py

---
+++ addons/sms/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'SMS gateway',
+    'author': 'Odoo S.A.',
+    'category': 'Tools',
+    'summary': 'SMS Text Messaging',
+    'description': """
+This module gives a framework for SMS text messaging
+----------------------------------------------------
+
+The service is provided by the In App Purchase actpy platform.
+""",
+    'depends': ['base', 'iap', 'mail'],
+    'data': [
+        'wizard/send_sms_views.xml',
+        'views/res_partner_views.xml',
+        'views/templates.xml',
+    ],
+    'qweb': [
+        'static/src/xml/sms_widget.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
