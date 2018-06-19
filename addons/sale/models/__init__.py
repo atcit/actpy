2f80b1edd49e58b6bd4a686449fea3b752758ed3
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:38:53 +0200

    Create new addons file : addons/sale/models/__init__.py

---
+++ addons/sale/models/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import analytic
+from . import account_invoice
+from . import product_pricelist
+from . import product_product
+from . import product_template
+from . import res_company
+from . import res_partner
+from . import sale
+from . import res_config_settings
+from . import sale_layout
+from . import sales_team
+from . import ir_model_fields
