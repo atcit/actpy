422a1b5ca82dcc38e5bfeea3b45a6ba3222db7e0
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:02:43 +0200

    Create new addons file : addons/base_import_module/__manifest__.py

---
+++ addons/base_import_module/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Base import module',
+    'author' : 'Odoo S.A',
+    'description': """
+Import a custom data module
+===========================
+
+This module allows authorized users to import a custom data module (.xml files and static assests)
+for customization purpose.
+""",
+    'category': 'Extra Tools',
+    'depends': ['web'],
+    'installable': True,
+    'auto_install': False,
+    'data': ['views/base_import_module_view.xml'],
+}
