930b14cc782f3cc9afba68364172315aa519214e
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:49:35 +0200

    Create new addons file : addons/base_gengo/models/__init__.py

---
+++ addons/base_gengo/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import res_config_settings
+from . import ir_translation
+from . import res_company
