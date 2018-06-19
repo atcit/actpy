76f58cc1beb24e41828030f3769233b5ca3f1f03
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:14:03 +0200

    Create new addons file : addons/l10n_be_hr_payroll/__manifest__.py

---
+++ addons/l10n_be_hr_payroll/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Belgium - Payroll',
+    'author' : 'Odoo S.A',
+    'category': 'Localization',
+    'depends': ['hr_payroll'],
+    'description': """
+Belgian Payroll Rules.
+======================
+
+    * Employee Details
+    * Employee Contracts
+    * Passport based Contract
+    * Allowances/Deductions
+    * Allow to configure Basic/Gross/Net Salary
+    * Employee Payslip
+    * Monthly Payroll Register
+    * Integrated with Leaves Management
+    * Salary Maj, ONSS, Withholding Tax, Child Allowance, ...
+    """,
+
+    'website': 'https://actpy.com/page/accounting',
+    'data': [
+        'views/l10n_be_hr_payroll_view.xml',
+        'data/l10n_be_hr_payroll_data.xml',
+        'data/hr.salary.rule.csv',
+    ],
+}
