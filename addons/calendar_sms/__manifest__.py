0bdfc9a77a661154984fd0b1c800558492740725
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:39:22 +0200

    Create new addons file : addons/calendar_sms/__manifest__.py

---
+++ addons/calendar_sms/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Calendar SMS",
+    'author' : 'Odoo S.A',
+    'summary': 'Send text messages as event reminders',
+    'description': "Send text messages as event reminders",
+    'category': 'Hidden',
+    'version': '1.0',
+    'depends': ['calendar', 'sms'],
+    'data': [
+        'views/calendar_views.xml',
+    ],
+    'application': False,
+    'auto_install': True,
+}
