038d67941dee17668bf261e5f19449dcea9d4156
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:31:41 +0200

    Create new addons file : addons/sale_mrp/tests/__init__.py

---
+++ addons/sale_mrp/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import test_move_explode
+from . import test_sale_mrp_flow
+from . import test_sale_mrp_lead_time
