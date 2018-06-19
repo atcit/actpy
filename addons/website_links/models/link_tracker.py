b83ad88a1d6cb6d816b633a89b91d28c12a4be7f
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:44:32 +0200

    Create new addons file : addons/website_links/models/link_tracker.py

---
+++ addons/website_links/models/link_tracker.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from actpy import models, fields
+
+
+class LinkTracker(models.Model):
+    _inherit = "link.tracker"
+
+    website_id = fields.Many2one('website', string='Website',
+                                 help='Define website in which this '
+                                      'link will enabled!')
