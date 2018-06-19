89c39d5259626792bb6a68a640aa79351f4c7038
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:53:16 +0200

    Create new addons file : addons/stock/__init__.py

---
+++ addons/stock/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import controllers
+from . import models
+from . import report
+from . import wizard
+from actpy import api
+from actpy.api import Environment, SUPERUSER_ID
+
+
+# TODO: Apply proper fix & remove in master
+def pre_init_hook(cr):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['ir.model.data'].search([
+        ('model', 'like', '%stock%'),
+        ('module', '=', 'stock')
+    ]).unlink()
+
+
+def post_init_check(cr, registery):
+    env = Environment(cr, SUPERUSER_ID, {})
+    move_obj = env['stock.move']
+    move_ids = move_obj.search([])
+    move_ids.set_move_type()
+    done_moves = move_obj.search([('state', '=', 'done')], order='date')
+    done_moves.check_move_bal_qty()
+    return True
