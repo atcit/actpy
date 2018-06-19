911a960a91a5646fd4e93636462608d2d632008e
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:34:32 +0200

    Create new addons file : addons/account_budget/tests/__init__.py

---
+++ addons/account_budget/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import common
+from . import test_theoreticalamount
+from . import test_account_budget
