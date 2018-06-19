cfe4f55c2ed17d592136147a326db158c7e6ea64
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:36:52 +0200

    Create new addons file : addons/website_crm_partner_assign/__manifest__.py

---
+++ addons/website_crm_partner_assign/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Resellers',
+    'author': 'Odoo S.A.',
+    'category': 'Sales',
+    'website': 'https://actpy.com/page/website-builder',
+    'summary': 'Publish Your Channel of Resellers',
+    'version': '1.0',
+    'description': """
+Publish and Assign Partner
+==========================
+        """,
+    'depends': ['base_geolocalize', 'crm', 'account',
+                'website_partner', 'website_google_map', 'portal'],
+    'data': [
+        'data/crm_partner_assign_data.xml',
+        'security/ir.model.access.csv',
+        'security/ir_rule.xml',
+        'wizard/crm_forward_to_partner_view.xml',
+        'views/res_partner_views.xml',
+        'views/crm_lead_views.xml',
+        'views/website_crm_partner_assign_templates.xml',
+        'report/crm_lead_report_view.xml',
+        'report/crm_partner_report_view.xml',
+    ],
+    'demo': [
+        'data/res_partner_demo.xml',
+        'data/crm_lead_demo.xml',
+        'data/res_partner_grade_demo.xml',
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+    'installable': True,
+}
