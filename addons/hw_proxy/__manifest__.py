6f4dfb9d75f47f988d85eabd490d4e289216a6b5
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:57:51 +0200

    Create new addons file : addons/hw_proxy/__manifest__.py

---
+++ addons/hw_proxy/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Hardware Proxy',
+    'author' : 'Odoo S.A',
+    'category': 'Point Of Sale',
+    'sequence': 6,
+    'summary': 'Connect the Web Client to Hardware Peripherals',
+    'website': 'https://actpy.com/page/point-of-sale',
+    'description': """
+Hardware Poxy
+=============
+
+This module allows you to remotely use peripherals connected to this server.
+
+This modules only contains the enabling framework. The actual devices drivers
+are found in other modules that must be installed separately. 
+
+""",
+}
