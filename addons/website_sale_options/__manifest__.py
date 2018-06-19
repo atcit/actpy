c16bf5cf023a2d0c06ecadc03dba589a1199579e
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:31:35 +0200

    Create new addons file : addons/website_sale_options/__manifest__.py

---
+++ addons/website_sale_options/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'eCommerce Optional Products',
+    'author': 'Odoo S.A.',
+    'category': 'Website',
+    'version': '1.0',
+    'website': 'https://actpy.com/page/e-commerce',
+    'description': """
+Odoo E-Commerce
+==================
+
+        """,
+    'depends': ['website_sale'],
+    'data': [
+        'views/product_views.xml',
+        'views/website_sale_options_templates.xml',
+    ],
+    'demo': [
+        'data/product_demo.xml',
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+    'installable': True,
+}
