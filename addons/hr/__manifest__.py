be8df1227d0a026f4167cbb563a7e097b8864dd9
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:40:34 +0200

    Create new addons file : addons/hr/__manifest__.py

---
+++ addons/hr/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Employee Directory',
+    'author' : 'Odoo S.A',
+    'version': '1.1',
+    'category': 'Human Resources',
+    'sequence': 75,
+    'summary': 'Jobs, Departments, Employees Details',
+    'description': "",
+    'website': 'https://actpy.com/page/employees',
+    'images': [
+        'images/hr_department.jpeg',
+        'images/hr_employee.jpeg',
+        'images/hr_job_position.jpeg',
+        'static/src/img/default_image.png',
+    ],
+    'depends': [
+        'base_setup',
+        'mail',
+        'resource',
+        'web',
+    ],
+    'data': [
+        'security/hr_security.xml',
+        'security/ir.model.access.csv',
+        'views/hr_views.xml',
+        'views/hr_templates.xml',
+        'views/res_config_settings_views.xml',
+        'data/hr_data.xml',
+    ],
+    'demo': [
+        'data/hr_demo.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [],
+}
