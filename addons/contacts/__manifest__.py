30af59a7917157fd72f2dd73f5781af2f9350c4a
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:46:48 +0200

    Create new addons file : addons/contacts/__manifest__.py

---
+++ addons/contacts/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Contacts Directory',
+    'author' : 'Odoo S.A',
+    'category': 'Tools',
+    'summary': 'Customers, Vendors, Partners,...',
+    'description': """
+This module gives you a quick view of your contacts directory, accessible from your home page.
+You can track your vendors, customers and other contacts.
+""",
+    'depends': ['base'],
+    'data': [
+        'views/contact_views.xml',
+    ],
+    'application': True,
+}
