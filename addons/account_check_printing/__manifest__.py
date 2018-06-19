ec5476a4486256a96fa0cff6147928ef843d3528
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:32:03 +0200

    Create new addons file : addons/account_check_printing/__manifest__.py

---
+++ addons/account_check_printing/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Check Printing Base',
+    'author': 'Odoo S.A',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Check printing commons',
+    'description': """
+This module offers the basic functionalities to make payments by printing checks.
+It must be used as a dependency for modules that provide country-specific check templates.
+The check settings are located in the accounting journals configuration page.
+    """,
+    'website': 'https://actpy.com/page/accounting',
+    'depends': ['account'],
+    'data': [
+        'data/account_check_printing_data.xml',
+        'views/account_journal_views.xml',
+        'views/account_payment_views.xml',
+        'wizard/print_prenumbered_checks_views.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
