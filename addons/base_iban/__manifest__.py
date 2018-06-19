54d27e4dd5b9f6c471fb49b526f2031db22e624e
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:46:32 +0200

    Create new addons file : addons/base_iban/__manifest__.py

---
+++ addons/base_iban/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'IBAN Bank Accounts',
+    'author': 'Odoo S.A',
+    'category': 'Extra Tools',
+    'description': """
+This module installs the base for IBAN (International Bank Account Number) bank accounts and checks for it's validity.
+======================================================================================================================
+
+The ability to extract the correctly represented local accounts from IBAN accounts
+with a single statement.
+    """,
+    'depends': ['account'],
+    'demo': ['data/res_partner_bank_demo.xml'],
+}
