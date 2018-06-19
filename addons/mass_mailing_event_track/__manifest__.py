a27ece6deaa2fb5347fa5fd61ba26d8cef7553d3
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:13:15 +0200

    Create new addons file : addons/mass_mailing_event_track/__manifest__.py

---
+++ addons/mass_mailing_event_track/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Mass mailing on track speakers',
+    'author' : 'Odoo S.A',
+    'category': 'Hidden',
+    'version': '1.0',
+    'description':
+        """
+Mass mail event track speakers
+==============================
+
+Bridge module adding UX requirements to ease mass mailing of event track speakers.
+        """,
+    'depends': ['website_event_track', 'mass_mailing'],
+    'data': [
+        'views/event_views.xml'
+    ],
+    'auto_install': True,
+}
