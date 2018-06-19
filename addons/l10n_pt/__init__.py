f27ea6609c9de328a16a4b8273d11f3e8cbb0ad0
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:42:58 +0200

    Create new addons file : addons/l10n_pt/__init__.py

---
+++ addons/l10n_pt/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) 2012 Thinkopen Solutions, Lda. All Rights Reserved
+# http://www.thinkopensolutions.com.
