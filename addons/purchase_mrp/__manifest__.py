dd08332406615817a48a2c7359fc0a943cc7e8d8
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:31:52 +0200

    Create new addons file : addons/purchase_mrp/__manifest__.py

---
+++ addons/purchase_mrp/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Purchase and MRP Management',
+    'author': 'Odoo S.A.',
+    'version': '1.0',
+    'category': 'Hidden',
+    'description': """
+This module provides facility to the user to install mrp and purchase modules at a time.
+========================================================================================
+
+It is basically used when we want to keep track of production orders generated
+from purchase order.
+    """,
+    'website': 'https://actpy.com/page/manufacturing',
+    'depends': ['mrp', 'purchase'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
