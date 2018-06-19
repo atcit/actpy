eb79469b7000af3356a4a1ffc5a2f51873f95027
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:11:59 +0200

    Create new addons file : addons/survey_crm/__manifest__.py

---
+++ addons/survey_crm/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Survey CRM',
+    'author': 'Odoo S.A.',
+    'version': '2.0',
+    'category': 'Marketing',
+    'complexity': 'easy',
+    'website': 'https://actpy.com/page/survey',
+    'description': """
+Survey - CRM (bridge module)
+=================================================================================
+This module adds a Survey mass mailing button inside the more option of lead/customers views
+""",
+    'depends': ['crm', 'survey'],
+    'data': [
+        'views/survey_crm_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True
+}
