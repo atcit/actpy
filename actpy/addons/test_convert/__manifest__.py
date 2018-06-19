d3e41f9f30a74ce513679070dc035ce9406fb2e0
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 09:55:25 +0200

    Create new API file : actpy/addons/test_convert/__manifest__.py and its directory

---
+++ actpy/addons/test_convert/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'test_convert',
+    'author': 'Odoo S.A',
+    'description': "Data for xml conversion tests",
+    'version': '0.0.1',
+}
