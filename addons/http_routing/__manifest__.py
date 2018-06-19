bd94bb82a25ba9477603297309952352e2654bb6
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:44:24 +0200

    Create new addons file : addons/http_routing/__manifest__.py

---
+++ addons/http_routing/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Web Routing',
+    'author' : 'Odoo S.A',
+    'summary': 'Web Routing',
+    'sequence': '9100',
+    'category': 'Hidden',
+    'description': """
+Proposes advanced routing options not available in web or base to keep
+base modules simple.
+""",
+    'data': [
+    ],
+    'depends': ['web'],
+}
