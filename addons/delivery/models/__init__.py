9e3a77a1f18297fb781bb4fa72dc149a8f832768
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:49:15 +0200

    Create new addons file : addons/delivery/models/__init__.py

---
+++ addons/delivery/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import res_config_settings
+from . import delivery_carrier
+from . import delivery_grid
+from . import product_template
+from . import product_packaging
+from . import sale_order
+from . import partner
+from . import stock_picking
+from . import stock_move
