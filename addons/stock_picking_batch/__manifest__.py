197c8adff4c3653ccd852c725c6a8a5f7de637fb
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:34:22 +0200

    Create new addons file : addons/stock_picking_batch/__manifest__.py

---
+++ addons/stock_picking_batch/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Warehouse Management: Batch Picking',
+    'author': 'Odoo S.A.',
+    'version': '1.0',
+    'category': 'Warehouse',
+    'description': """
+This module adds the batch picking option in warehouse management
+=================================================================
+    """,
+    'website': 'https://actpy.com/page/warehouse',
+    'depends': ['stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/stock_picking_batch_views.xml',
+        'data/stock_picking_batch_data.xml',
+        'wizard/stock_picking_to_batch_views.xml',
+    ],
+    'demo': [
+        'data/stock_picking_batch_demo.xml',
+    ],
+    'installable': True,
+}
