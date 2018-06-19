d8eb7d63b5c889b909b1e5bf9243a81b71d54a7c
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:31:53 +0200

    Create new addons file : addons/l10n_hr/__init__.py

---
+++ addons/l10n_hr/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+# Author: Goran Kliska
+# mail:   goran.kliska(AT)slobodni-programi.hr
+# Copyright (C) 2011- Slobodni programi d.o.o., Zagreb
