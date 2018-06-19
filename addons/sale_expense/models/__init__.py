f71daa5ae1702c7315845b4ebadafd9a788f2f25
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:13:56 +0200

    Create new addons file : addons/sale_expense/models/__init__.py

---
+++ addons/sale_expense/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import analytic
+from . import hr_expense
+from . import product_template
