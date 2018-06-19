5ad87ed5364f0709cb39be94f1bd5c04f8c5f67b
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:00:35 +0200

    Create new addons file : addons/pos_reprint/__manifest__.py

---
+++ addons/pos_reprint/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Point of Sale Receipt Reprinting',
+    'author': 'Odoo S.A.',
+    'version': '1.0',
+    'category': 'Point of Sale',
+    'sequence': 6,
+    'summary': 'Allow cashier to reprint receipts',
+    'description': """
+
+Allow cashier to reprint receipts
+
+""",
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_reprint_templates.xml',
+        'views/pos_config_views.xml'
+    ],
+    'qweb': [
+        'static/src/xml/reprint.xml',
+    ],
+    'installable': True,
+    'website': 'https://actpy.com/page/point-of-sale',
+}
