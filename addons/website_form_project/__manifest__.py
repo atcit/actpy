1e2d18e2a2daf9df2425075e956400af507e63a8
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:59:10 +0200

    Create new addons file : addons/website_form_project/__manifest__.py

---
+++ addons/website_form_project/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Website Form - Project',
+    'author': 'Odoo S.A.',
+    'category': 'Project',
+    'summary': 'Create Tasks From Contact Form',
+    'version': '1.0',
+    'description': """
+Odoo Contact Form
+=================
+
+        """,
+    'depends': ['website_form', 'project'],
+    'data': [
+        'data/website_form_project_data.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
