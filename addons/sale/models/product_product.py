9af42890e1a0ff0844f323e2ec216091d3d65d89
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:38:54 +0200

    Create new addons file : addons/sale/models/product_product.py

---
+++ addons/sale/models/product_product.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from actpy import api, fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.multi
+    def _sales_count(self):
+        r = {}
+        if not self.user_has_groups('sales_team.group_sale_salesman'):
+            return r
+        domain = [
+            ('state', 'in', ['sale', 'done']),
+            ('product_id', 'in', self.ids),
+        ]
+        for group in self.env['sale.report'].read_group(domain, ['product_id', 'product_uom_qty'], ['product_id']):
+            r[group['product_id'][0]] = group['product_uom_qty']
+        for product in self:
+            product.sales_count = r.get(product.id, 0)
+        return r
+
+    sales_count = fields.Integer(compute='_sales_count', string='# Sales')
