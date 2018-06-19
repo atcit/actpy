4bc51ea0d13c147f625643cb1f4a9c5b077e874d
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 09:55:23 +0200

    Create new API file : actpy/addons/test_uninstall/__manifest__.py and its directory

---
+++ actpy/addons/test_uninstall/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'test-uninstall',
+    'author': 'Odoo S.A',
+    'version': '0.1',
+    'category': 'Tests',
+    'description': """A module to test the uninstall feature.""",
+    'depends': ['base'],
+    'data': ['ir.model.access.csv'],
+    'installable': True,
+    'auto_install': False,
+}
