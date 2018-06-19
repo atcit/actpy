3d28eb88960439aa4138611454dce26d99019cd6
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:49:46 +0200

    Create new addons file : addons/website_google_map/__manifest__.py

---
+++ addons/website_google_map/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Website Google Map',
+    'author': 'Odoo S.A.',
+    'category': 'Website',
+    'summary': '',
+    'version': '1.0',
+    'description': """
+Odoo Website Google Map
+==========================
+
+        """,
+    'depends': ['base_geolocalize', 'website_partner'],
+    'data': [
+        'views/google_map_templates.xml',
+    ],
+    'installable': True,
+}
