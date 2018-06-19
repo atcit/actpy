5cc00f8b19be6a0e1796ff4a47f865331c10a6c5
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:34:21 +0200

    Create new addons file : addons/stock_picking_batch/wizard/__init__.py

---
+++ addons/stock_picking_batch/wizard/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import stock_picking_to_batch
+from . import stock_immediate_transfer
