d9ed3d8f40264df296280d8ef29ab386de3d1fdf
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:10:36 +0200

    Create new addons file : addons/website_sale/models/sale_report.py

---
+++ addons/website_sale/models/sale_report.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of actpy. See LICENSE file for full copyright and licensing details.
+
+from actpy import api, fields, models
+
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+    website_id = fields.Many2one('website', 'Website', readonly=True)
+
+    def _select(self):
+        return \
+            super(SaleReport, self)._select() + ", s.website_id as website_id"
+
+    def _group_by(self):
+        res = super(SaleReport, self)._group_by()
+        return res + ',s.website_id'
