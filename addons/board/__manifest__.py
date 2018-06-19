9d1281fc9dbac0b9f2d41c18428835d03982f63d
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:32:49 +0200

    Create new addons file : addons/board/__manifest__.py

---
+++ addons/board/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Dashboards',
+    'author' : 'Odoo S.A',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'Create your custom dashboard',
+    'description': """
+Lets the user create a custom dashboard.
+========================================
+
+Allows users to create custom dashboard.
+    """,
+    'depends': ['base', 'web'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/board_views.xml',
+        'views/board_templates.xml',
+    ],
+    'qweb': ['static/src/xml/board.xml'],
+    'application': True,
+}
