583572aa6e33f24a1789d4864e295ee8d0773ded
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:38:32 +0200

    Create new addons file : addons/google_spreadsheet/models/res_config_settings.py

---
+++ addons/google_spreadsheet/models/res_config_settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from actpy import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    google_drive_uri_copy = fields.Char(related='google_drive_uri', string='URI', help="The URL to generate the authorization code from Google")
