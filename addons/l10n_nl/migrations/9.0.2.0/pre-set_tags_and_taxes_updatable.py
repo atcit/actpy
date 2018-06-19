7a331b6a8946bf7618236018695780574310f9b6
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:53:36 +0200

    Create new addons file : addons/l10n_nl/migrations/9.0.2.0/pre-set_tags_and_taxes_updatable.py

---
+++ addons/l10n_nl/migrations/9.0.2.0/pre-set_tags_and_taxes_updatable.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import actpy
+
+def migrate(cr, version):
+    registry = actpy.registry(cr.dbname)
+    from actpy.addons.account.models.chart_template import migrate_set_tags_and_taxes_updatable
+    migrate_set_tags_and_taxes_updatable(cr, registry, 'l10n_nl')
