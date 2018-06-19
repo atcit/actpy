76998566fd8c55916c88d2e02386d69856d657c4
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:39:52 +0200

    Create new addons file : addons/l10n_fr/__init__.py

---
+++ addons/l10n_fr/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2008 JAILLET Simon - CrysaLEAD - www.crysalead.fr
+
+from . import models
+
+
+def _preserve_tag_on_taxes(cr, registry):
+    from actpy.addons.account.models.chart_template import preserve_existing_tags_on_taxes
+    preserve_existing_tags_on_taxes(cr, registry, 'l10n_fr')
