1b6ed22248b3cfe86ffa58f2b46a1d8b4c9703a1
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:40:52 +0200

    Create new addons file : addons/mail/__init__.py

---
+++ addons/mail/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import wizard
+from . import controllers
+from .tests import test_mail_model
+
+
+def post_init(cr, registry):
+    from actpy import api, SUPERUSER_ID
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['publisher_warranty.contract'].update_notification()
