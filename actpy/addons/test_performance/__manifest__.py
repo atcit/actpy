faed5d8a64f1be8ce549bb9fd4f1597b77aedb75
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 09:55:01 +0200

    Create new API file : actpy/addons/test_performance/__manifest__.py and its directory

---
+++ actpy/addons/test_performance/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Test Performance",
+    'author': 'Odoo S.A',
+    'version': "1.0",
+    'category': "Hidden",
+    'depends': ['base', 'mail'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
