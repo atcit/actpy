400f36cb2eefe7e6a6e373b8e34b8f675fce77a2
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:00:03 +0200

    Create new addons file : addons/account_cash_flow/models/account.py

---
+++ addons/account_cash_flow/models/account.py
@@ -0,0 +1,15 @@
+# Part of actpy See LICENSE file for full copyright and licensing details.
+
+from actpy import fields, models
+
+
+class Account(models.Model):
+    _inherit = 'account.account.type'
+
+    activity_type = fields.Selection(
+        [('operation_income', 'Operation-Income'),
+         ('operation_expense', 'Operation-Expense'),
+         ('operation_current_asset', 'Operation-Current Asset'),
+         ('operation_current_liability', 'Operation-Cuurent Liability'),
+         ('investing', 'Investing'),
+         ('financing', 'Financing')], string='Activity Type')
