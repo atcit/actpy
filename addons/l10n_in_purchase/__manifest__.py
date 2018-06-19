195bfdf8e050fba9c5bff50c6f0bfc8f2270675b
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:34:59 +0200

    Create new addons file : addons/l10n_in_purchase/__manifest__.py

---
+++ addons/l10n_in_purchase/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Indian - Purchase Report(GST)',
+    'author' : 'Odoo S.A',
+    'version': '1.0',
+    'description': """GST Purchase Report""",
+    'category': 'Localization',
+    'depends': [
+        'l10n_in',
+        'purchase',
+    ],
+    'data': [
+        'views/report_purchase_order.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
