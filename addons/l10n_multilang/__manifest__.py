9c7e77955d0199248d047b23cfe6f0fd7df2b09a
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:35:27 +0200

    Create new addons file : addons/l10n_multilang/__manifest__.py

---
+++ addons/l10n_multilang/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Multi Language Chart of Accounts',
+    'author' : 'Odoo S.A',
+    'version': '1.1',
+    'category': 'Localization',
+    'description': """
+    * Multi language support for Chart of Accounts, Taxes, Tax Codes, Journals,
+      Accounting Templates, Analytic Chart of Accounts and Analytic Journals.
+    * Setup wizard changes
+        - Copy translations for COA, Tax, Tax Code and Fiscal Position from
+          templates to target objects.
+    """,
+    'depends': ['account'],
+}
