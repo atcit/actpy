4dc9b1a31428fc6d91b439c592701c4593325f89
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 09:55:02 +0200

    Create new API file : actpy/addons/test_impex/__manifest__.py and its directory

---
+++ actpy/addons/test_impex/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'test-import-export',
+    'author': 'Odoo S.A',
+    'version': '0.1',
+    'category': 'Tests',
+    'description': """A module to test import/export.""",
+    'depends': ['base'],
+    'data': ['ir.model.access.csv'],
+    'installable': True,
+    'auto_install': False,
+    'test': [
+        'tests/test_import_reference.yml',
+        'tests/test_import_menuitem.yml',
+    ]
+}
