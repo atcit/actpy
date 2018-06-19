b41b044065ed6686f09a344bb48711da3032289b
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:44:49 +0200

    Create new addons file : addons/fleet/models/__init__.py

---
+++ addons/fleet/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import fleet_vehicle
+from . import fleet_vehicle_cost
+from . import fleet_vehicle_model
