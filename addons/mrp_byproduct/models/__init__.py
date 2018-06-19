8045c56bdb24347f1d98c69fcf1403e701417962
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:36:28 +0200

    Create new addons file : addons/mrp_byproduct/models/__init__.py

---
+++ addons/mrp_byproduct/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import mrp_bom
+from . import mrp_production
+from . import mrp_subproduct
+from . import stock_move
