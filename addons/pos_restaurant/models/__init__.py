ac72920b9d642a0b2ef923a5371ccc1cf6e14b4f
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:41:44 +0200

    Create new addons file : addons/pos_restaurant/models/__init__.py

---
+++ addons/pos_restaurant/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import pos_config
+from . import pos_order
+from . import pos_restaurant
