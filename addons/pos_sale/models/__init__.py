83e2cb327e117db956c5ea46f4cb653eb7493947
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:44:16 +0200

    Create new addons file : addons/pos_sale/models/__init__.py

---
+++ addons/pos_sale/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import pos_config
+from . import crm_team
+from . import pos_session
