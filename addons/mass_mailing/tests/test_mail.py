ec98bed3631845ee8c73abc42f9d218ebc907e69
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:55:22 +0200

    Create new addons file : addons/mass_mailing/tests/test_mail.py

---
+++ addons/mass_mailing/tests/test_mail.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from actpy.addons.mail.tests.common import TestMail
+
+
+class test_message_compose(TestMail):
+
+    def test_OO_mail_mail_tracking(self):
+        """ Tests designed for mail_mail tracking (opened, replied, bounced) """
+        pass
