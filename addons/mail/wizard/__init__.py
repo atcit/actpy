22b1d0a06e1544aec248463f3e1ef50245a17357
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:41:01 +0200

    Create new addons file : addons/mail/wizard/__init__.py

---
+++ addons/mail/wizard/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import invite
+from . import mail_compose_message
+from . import email_template_preview
+from . import base_module_uninstall
+from . import mass_mail
+
