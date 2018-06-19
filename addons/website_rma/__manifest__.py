dddc8cb5014fefbee1fa4727bfeaa1f3ca017d44
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:53:56 +0200

    Create new addons file : addons/website_rma/__manifest__.py

---
+++ addons/website_rma/__manifest__.py
@@ -0,0 +1,28 @@
+# Part of actpy See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Website Return Merchandise Authorizationing',
+    'version': '1.0',
+    'author': 'actpy',
+    'website': 'https://actpy.com',
+    'description': """
+        Return Merchandise Authorizationing is part of the process of a
+        consumer returning a product to receive a refund or replacement
+        during the product's warranty period.
+    """,
+    'category': 'Sales',
+    'depends': ['rma', 'website_sale', 'portal'],
+    'data': [
+        'wizard/stock_return_web_view.xml',
+        'views/rma_request_view.xml',
+        'views/return_portal_template.xml',
+        'views/templates.xml',
+
+    ],
+    'demo': [
+        'demo/sale_order_demo.xml'
+    ],
+    'images': [
+        'static/description/website-rma-app-banner.jpg',
+    ],
+}
