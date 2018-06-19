a46811bb804f7d06f22109e8271eb51e99248801
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:58:22 +0200

    Create new addons file : addons/base_setup/__manifest__.py

---
+++ addons/base_setup/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Initial Setup Tools',
+    'author' : 'Odoo S.A',
+    'version': '1.0',
+    'category': 'Hidden',
+    'description': """
+This module helps to configure the system at the installation of a new database.
+================================================================================
+
+Shows you a list of applications features to install from.
+
+    """,
+    'depends': ['base', 'web'],
+    'data': [
+        'views/res_config_settings_views.xml',
+        'views/res_partner_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
