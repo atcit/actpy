919a8429eaf58059074c3b1392f6a1cb08935031
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:13:30 +0200

    Create new addons file : addons/crm_phone_validation/models/__init__.py

---
+++ addons/crm_phone_validation/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+from . import res_partner
+from . import crm_lead
+from . import res_config_settings
