349f34c23741e5d422db5a32a4dbc6ed8367245d
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:00:24 +0200

    Create new addons file : addons/l10n_syscohada/__init__.py

---
+++ addons/l10n_syscohada/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) 2010-2011 BAAMTU SARL (<http://www.baamtu.sn>).
+# contact: leadsn@baamtu.com
