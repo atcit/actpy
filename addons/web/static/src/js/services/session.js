1434c9abf83cb916c0db8f1fe31d0102ad43b3a0
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:04:03 +0200

    Create new addons file : addons/web/static/src/js/services/session.js

---
+++ addons/web/static/src/js/services/session.js
@@ -0,0 +1,12 @@
+actpy.define('web.session', function (require) {
+"use strict";
+
+var Session = require('web.Session');
+var modules = actpy._modules;
+
+var session = new Session(undefined, undefined, {modules: modules, use_cors: false});
+session.is_bound = session.session_bind();
+
+return session;
+
+});
