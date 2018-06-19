8375518ef43bf72c438327c2c6f945df6ee19f8f
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 13:02:07 +0200

    Create new addons file : addons/website_event_sale/__manifest__.py

---
+++ addons/website_event_sale/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Online Event's Tickets",
+    'author': 'Odoo S.A.',
+    'category': 'Marketing',
+    'summary': "Manage Events and Sell Tickets Online",
+    'website': 'https://actpy.com/page/events',
+    'description': """
+Online Event's Tickets
+======================
+
+        """,
+    'depends': ['website_event', 'event_sale', 'website_sale'],
+    'data': [
+        'data/event_data.xml',
+        'views/event_templates.xml',
+        'security/ir.model.access.csv',
+        'security/website_event_sale_security.xml',
+    ],
+    'auto_install': True
+}
