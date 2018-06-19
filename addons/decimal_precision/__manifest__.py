2c75a4cca0a897164d15ca2d73c7c97f310b2bb9
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:52:16 +0200

    Create new addons file : addons/decimal_precision/__manifest__.py

---
+++ addons/decimal_precision/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Decimal Precision Configuration',
+    'author' : 'Odoo S.A',
+    'description': """
+Configure the price accuracy you need for different kinds of usage: accounting, sales, purchases.
+=================================================================================================
+""",
+    'version': '0.1',
+    'depends': ['base'],
+    'category' : 'Hidden',
+    'data': [
+        'views/decimal_precision_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'installable': True,
+}
