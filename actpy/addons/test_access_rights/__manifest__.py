893f3e631f9253263f9e17dad9060c3779c02baa
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 09:55:24 +0200

    Create new API file : actpy/addons/test_access_rights/__manifest__.py and its directory

---
+++ actpy/addons/test_access_rights/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'test of access rights and rules',
+    'author': 'Odoo S.A',
+    'description': "Testing of access restrictions",
+    'version': '0.0.1',
+    'data': ['ir.model.access.csv'],
+}
