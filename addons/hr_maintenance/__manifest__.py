797c865de0fbba54d35081de60e02e34e046010e
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:34:58 +0200

    Create new addons file : addons/hr_maintenance/__manifest__.py

---
+++ addons/hr_maintenance/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'HR - Equipments',
+    'author' : 'Odoo S.A',
+    'version': '1.0',
+    'sequence': 125,
+    'description': """
+        Bridge between HR and Maintenance.""",
+    'depends': ['hr', 'maintenance'],
+    'summary': 'Equipments, Assets, Internal Hardware, Allocation Tracking',
+    'data': [
+        'security/equipment.xml',
+        'views/maintenance_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto-install': True,
+}
