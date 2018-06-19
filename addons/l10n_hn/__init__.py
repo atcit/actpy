8934a99df50fdba46f892a768f13e18c0243abfc
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:40:36 +0200

    Create new addons file : addons/l10n_hn/__init__.py

---
+++ addons/l10n_hn/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2009-2010 Salvatore J. Trimarchi <salvatore@trimarchi.co.cc>
+# (http://salvatoreweb.co.cc)
