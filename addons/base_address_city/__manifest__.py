e1ac2027873f7e2a2316c88dd4797728d90733e3
Elbagoury <dr.elbagoury@hotmail.com>
2018-06-19 12:37:40 +0200

    Create new addons file : addons/base_address_city/__manifest__.py

---
+++ addons/base_address_city/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'City Addresses',
+    'author' : 'Odoo S.A',
+    'summary': 'Add a many2one field city on addresses',
+    'sequence': '19',
+    'category': 'Base',
+    'complexity': 'easy',
+    'description': """
+City Management in Addresses
+============================
+
+This module allows to enforce users to choose the city of a partner inside a given list instead of a free text field.
+        """,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_city_view.xml',
+        'views/res_country_view.xml',
+    ],
+    'depends': ['base'],
+}
