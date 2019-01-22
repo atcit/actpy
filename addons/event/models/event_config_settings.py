f9c1e0e5e5bdfeb590852c0494e9d06293ecfb68
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2019-01-22 16:20:00 +0300

    [ADD] event_config_settings.py

---
+++ addons/event/models/event_config_settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from actpy import fields, models
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    module_event_sale = fields.Boolean("Tickets")
+    module_website_event_track = fields.Boolean("Tracks and Agenda")
+    module_website_event_questions = fields.Boolean("Registration Survey")
+    module_website_event_sale = fields.Boolean("Online Ticketing")
