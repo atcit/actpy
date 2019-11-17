f189316db16d78e9cfa1df0e7b6e0abf6fa1ce1b
Mohamed Elbagoury <dr.elbagoury@hotmail.com>
2019-11-17 23:00:00 +0300

    [ADD] test_forum_process.py

---
+++ addons/website_forum/tests/test_forum_process.py
@@ -0,0 +1,19 @@
+# Part of Odoo, actpy. See LICENSE file for full copyright and licensing details.
+
+import actpy.tests
+
+class TestUi(actpy.tests.HttpCase):
+
+    post_install = True
+    at_install = False
+
+    def test_01_admin_forum_tour(self):
+        self.phantom_js("/", "actpy.__DEBUG__.services['web_tour.tour'].run('question')", "actpy.__DEBUG__.services['web_tour.tour'].tours.question.ready", login="admin")
+
+    def test_02_demo_question(self):
+        with self.cursor() as test_cr:
+            env = self.env(cr=test_cr)
+            forum = env.ref('website_forum.forum_help')
+            demo = env.ref('base.user_demo')
+            demo.karma = forum.karma_post + 1
+        self.phantom_js("/", "actpy.__DEBUG__.services['web_tour.tour'].run('forum_question')", "actpy.__DEBUG__.services['web_tour.tour'].tours.forum_question.ready", login="demo")
